8681a17d396e08e6456004c684ef61b5755ba51e
Kemal Başaran <basarankemal2002@gmail.com>
2021-01-22 21:17:52 +0300

    s

---
+++ Generic2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Generic2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Kemal");
+            myList.Add("aysel");
+            myList.Add("mahmut");
+            myList.Add("arif");
+            myList.Add("melike");
+            Console.WriteLine(myList.Count);
+        }
+    }
+    class MyList<K>
+    {
+
+        K[] _array;
+        K[] _TempArray;
+        public MyList()
+        {
+            _array = new K[0];
+        }
+
+        public void Add(K item)
+        {
+            _TempArray = _array;
+            _array = new K[_array.Length + 1];
+            for (int i = 0; i < _TempArray.Length; i++)
+            {
+                _array[i] = _TempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+
+        }
+
+    }
+
+
+    }
+
+        
+    
---
+++ Metotlar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya ELması";
+
+            string[] meyveler = new string[] { };
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.aciklama = "Amasya ELması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "karpuz";
+            urun2.Fiyati=80;
+            urun2.aciklama = "Diyarbakı karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+            
+            
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.aciklama);
+                Console.WriteLine("--------------------------");
+            }
+
+            Console.WriteLine("--------------METODLAR--------------");
+
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("armut", "yeşil armut", 12,10);
+            sepetManager.Ekle2("elma","yeşil elma",12,9);
+            sepetManager.Ekle2("elma", "diyabakır karpuzu", 12,8);
+            
+
+        }
+    }
+    
+
+
+}
